b97601be1f54a31d471de6dd2c7c8fd98c19031b
abrar aboobacker <abraraboobackerkk@gmail.com>
2025-01-07 15:32:09 +0530

    feat - added tailwind.config.js file

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const App = () => {
   return (
    <main>
-    <h1 className='text-5xl text-orange-400 text-end font-bold' >
+    <h1 className='text-5xl text-orange-400 text-end font-bold ' >
   Hello world
     </h1>
    </main>
