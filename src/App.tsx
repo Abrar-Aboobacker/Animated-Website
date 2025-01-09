0a975ab76bf42911b82174143f20707ad9b53286
abrar aboobacker <abraraboobackerkk@gmail.com>
2025-01-09 17:10:45 +0530

    feat- added Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
-import React from 'react'
-import Hero from './components/Hero'
-import About from './components/About'
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
-   <main className='relative min-h-screen w-screen overflow-hidden' >
-    <Hero/>
-   <About/>
-   </main>
-  )
-}
+    <main className="relative min-h-screen w-screen overflow-hidden">
+      <Navbar />
+      <Hero />
+      <About />
+    </main>
+  );
+};
 
-export default App
+export default App;
